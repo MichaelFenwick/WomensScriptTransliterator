4ab60d3f931242c2329f154773e32d6dfde6dbca
Michael Fenwick <mike@mikefenwick.com>
2023-11-06 17:23:26 -0800

    Resolves #29
    Added some logic to fix how ellipses are handled in XML documents. This could be done better, but for now is good enough.

--- lib/transliterator/transliterator/structure/xml_transliterator.dart
+++ lib/transliterator/transliterator/structure/xml_transliterator.dart
@@ -19,9 +19,20 @@ class XmlTransliterator<S extends Language, T extends Language> extends Structur
     final XmlDocument inputCopy = input.copy();
     final TextBlockTransliterator<S, T> textBlockTransliterator = TextBlockTransliterator.fromTransliterator(this);
 
-    input.descendants.whereType<XmlElement>().where((XmlElement element) => element.name.local == 'div').forEach((XmlElement div) {
-      final Iterable<Result<Atom<TextBlock, XmlText>, S, T>?> results = textBlockTransliterator.transliterateAtoms<XmlText>(
-          div.descendants.whereType<XmlText>().map((XmlText node) => Atom<TextBlock, XmlText>(TextBlock(node.text), node)).toList());
+    input.descendants
+        .whereType<XmlElement>()
+        .where((XmlElement element) => <String>['p', 'div', 'text'].contains(element.name.local))
+        .forEach((XmlElement div) {
+      final Iterable<Result<Atom<TextBlock, XmlText>, S, T>?> results = textBlockTransliterator.transliterateAtoms<XmlText>(div.descendants
+          .whereType<XmlText>()
+          .map((XmlText node) => Atom<TextBlock, XmlText>(
+              TextBlock(node.value
+                  //FIXME: This is probably the right place for this logic (it needs to happen before the text gets to the SentenceTransliterator, otherwise a final period in the ellipsis might be moved to the sentence's start [see https://github.com/MichaelFenwick/WomensScriptTransliterator/issues/29]). However, this is pretty sloppily shoehorned in and should be pulled into its own method. Perhaps it could be better placed Paragraph transliterator).
+                  .replaceAll(RegExp(r'\.(&nbsp;?\.){2,4}'),
+                      Unicode.ellipsis) // Replace any poorly formatted ellipsis attempts to an actual ellipsis character.
+                  .replaceAll(RegExp('&nbsp;'), ' ')), // And replace any leftover encoded nbsp characters with the actual character
+              node))
+          .toList());
       for (final Result<Atom<TextBlock, XmlText>, S, T>? result in results) {
         if (result is ResultPair<Atom<TextBlock, XmlText>, S, T>) {
           result.target.context.replace(XmlText(result.target.content.content));
